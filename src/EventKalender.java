04cb21727d2e794c751aa806815a83003be94b50
Philipp <philipp.kropik@gmail.com>
2016-01-16 12:37:32 +0100

    fertig Kommentiert

--- src/EventKalender.java
+++ src/EventKalender.java
@@ -51,75 +51,76 @@ public class EventKalender {
 		{
 			if (event.getEintrittsPreis() >= min && event.getEintrittsPreis()<= max) // Findet alle Veranstaltungen deren Preis zumindest das Minimum aber gleichzeitig höchstens den Maximalpreis hat. 
 			{
-				ByPreis.add(event);
+				ByPreis.add(event);// gefundene Veranstaltungen werden der oben erstellten Arraylist hinzugefügt.
 			}
 		}
-		return ByPreis;
+		return ByPreis; //zurückgeben der Arraylist mit allen gefundenen Veranstaltungen. 
+		//Hier ist kein return null notwendig da wir im schlimmsten fall die leere ArrayList zurückgeben. 
 	}
 	
 	public ArrayList<Event> getMostExpensiveByOrt(String ort)
 	{
-		double max =0;
-		ArrayList<Event> ByOrt = new ArrayList<>();
-		ArrayList<Event> Expense =new ArrayList<>();
+		double max =0; //setzt dem maximalen Preis am Beginn auf 0. 
+		ArrayList<Event> ByOrt = new ArrayList<>(); // ArrayList für das finale Ergebniss
+		ArrayList<Event> Expense =new ArrayList<>(); // Hilfs ArrayList, wird mit allen im ersten Suchdurchlauf gefundenen Events befüllt. 
 		for (Event event : EL) 
 		{
-			if (event.getEintrittsPreis()>=max && event.getOrt().equals(ort))
+			if (event.getEintrittsPreis()>=max && event.getOrt().equals(ort)) // Prüft auf 2 Bedingungen: Ist das event teurer als der aktuelle höchste Preis & ist das Event am gesuchten Ort. 
 			{
-				max = event.getEintrittsPreis();
-				ByOrt.add(event);
+				max = event.getEintrittsPreis(); //setzt den aktuellen Preis als neuen höchsten Preis. 
+				ByOrt.add(event); // fügt das gefundenen Event der ArrayList ByOrt hinzu
 			}
 		}
 		for (Event event : ByOrt) {
-			if (event.getEintrittsPreis() == max)
+			if (event.getEintrittsPreis() == max) // Prüft alle Events im Hilfsarray darauf ob sie den höchsten gefundenen Preis haben. 
 			{
-				Expense.add(event);
+				Expense.add(event); // fügt die gefundenen Evens der auszugebenden ArrayList hinzu 
 			}
 		}
 
 		return Expense;
 	}
 	
-	public double getAvgPreisByOrt(String ort)
+	public double getAvgPreisByOrt(String ort) //Berechnen des durchschnittlichen eventpreises am gegebenen Ort
 	{
-		double kosten =0;
+		double kosten =0; 
 		int counter = 0;
 		for (Event event : EL) {
-			if (event.getOrt().equals(ort))
+			if (event.getOrt().equals(ort)) // Prüft ob der Ort des Events mit dem gesuchten Ort übereinstimmt.
 			{
-				kosten += event.getEintrittsPreis();
-				counter++;
+				kosten += event.getEintrittsPreis(); // Addiert die Kosten des Events hinzu
+				counter++; 
 			}
 		}
-		if (counter ==0 )
+		if (counter ==0 ) // Abfangen des "Div by zero" Fehlers, falls es keine Events gibt. 
 		{
 			System.out.println("Keine Events gefunden");
 			return -1;
 		}
 		else
 		{
-		return kosten/counter;
+		return kosten/counter; // Durchschnitspreis
 		}
 	}
 	
-	public HashMap<String, Integer> getNumberByOrt()
+	public HashMap<String, Integer> getNumberByOrt() // Gibt die Events Pro Ort zurück.
 	{
 		int zahl1 =0;
 		
-		HashMap<String, Integer> EventList =new HashMap<>();
-		for (Event event : EL) 
+		HashMap<String, Integer> EventList =new HashMap<>(); // Erstellt eine neue HashMap. Aufbau <Key, Value> In unserem fall <Ort, Anzahl der Events>
+		for (Event event : EL) 		
 		{
-		if (EventList.containsKey(event.getOrt()))
+		if (EventList.containsKey(event.getOrt())) // Wenn es den Ort in der HasMap bereits gibt, erhöhe den Value des Keys um eins
 		{
 			zahl1 = EventList.get(event.getOrt());
 			EventList.put(event.getOrt(),zahl1+1);
 		}
-		else 
+		else  // Wenn es den Ort in der Map noch nicht gibt, lege einen Eintrag für den Key an und setze den Value auf 1. 
 		{
 			EventList.put(event.getOrt(), 1);
 		}
 		
 		}
-	return EventList;
+	return EventList; 
 	}
 }
